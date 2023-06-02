8cd1b5c10db1e48aec0a1d584b1b51032e63c01e
Anandu-Vijayan <ananduvijayan8@gmail.com>
2023-06-02 17:30:34 +0530

    Login completed

--- Server/src/controllers/authentication.ts
+++ Server/src/controllers/authentication.ts
@@ -1,30 +1,64 @@
-import express from 'express'
+import express from "express";
 
-import { createUser, getUserByEmail } from '../db/users';
-import { authentication, random } from '../helpers';
+import { createUser, getUserByEmail } from "../db/users";
+import { authentication, random } from "../helpers";
 
-export const register = async (req:express.Request,res:express.Response) =>{
-    try {
-      const {email,password,username} = req.body;
-      if(!email || !password || !username){
-        return res.status(400)
-      }
-      const existUser = await getUserByEmail(email);
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
 
-      if(existUser){
-        return res.status(400).json("Email is already registered")
-      }
-      const salt = random();
-      const user = await createUser({
-        email,
-        username,
-        authentication:{
-            salt,
-            password:authentication(salt,password),
-        }
-      })
-      return res.status(200).json(user).end() 
-    } catch (error) {
-        return res.status(500).json({error:error.message})
+    if (!email || !password) {
+      return res.status(400).json("Invalide Username or Password");
     }
-}
+
+    const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+
+    if (!user) {
+      return res.status(400).json("User dosen't exist");
+    }
+
+    const expectedHash = authentication(user.authentication.salt, password);
+    
+    if (user.authentication.password != expectedHash) {
+      return res.status(400).json("Wrong Password"); 
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+    await user.save();
+
+    res.cookie('ANANDU', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.status(400);
+  }
+};
+
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password, username } = req.body;
+    if (!email || !password || !username) {
+      return res.status(400).json("please enter valid email or password");
+    }
+    const existUser = await getUserByEmail(email);
+
+    if (existUser) {
+      return res.status(400).json("Email is already registered");
+    }
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+    return res.status(200).json(user).end();
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
--- Server/src/router/authentication.ts
+++ Server/src/router/authentication.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 
-import { register } from '../controllers/authentication';
+import { login, register } from '../controllers/authentication';
 
 
 export default (router:express.Router) =>{
     router.post('/auth/register',register);
+    router.post('/auth/login',login);
 }
